a2d84a37d3ebbafddad2f41660456cccdc773580
dsoutofernandez <dsoutofernandez@eniac05.danielcastelao.org>
2015-01-26 09:34:03 +0100

    Commit Inicial

---
+++ src/mergecod/MergeCod.java
@@ -0,0 +1,39 @@
+package mergecod;
+
+public class MergeCod {
+
+    public static void main(String[] args) {
+        
+        int numeros[]=new int[5];
+        int aux;
+        
+        for (int i = 0; i<numeros.length -1; i++){
+        
+            numeros[i]=(int) (Math.random()*50)+1;
+        
+        }
+        
+       for (int i = 0; i< numeros.length-1; i++){
+
+                    for(int j = i+1; j<numeros.length; j++){
+
+                        if (numeros[i] < numeros[j]){
+
+                            aux = numeros[i];
+                            numeros[i] = numeros[j];
+                            numeros[j] = aux;
+                    }
+
+                }
+
+            }
+        
+        for(int i = 0; i<numeros.length -1;i++){
+            System.out.println(numeros[i]);
+        }
+        
+    }
+    
+}
+    
+}
